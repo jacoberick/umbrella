a72e15a1a1e61a2ce77106ca1e1d55361c06d46f
Jacob White <jacobewhitedev@gmail.com>
2021-11-21 21:07:33 -0600

    moved info to its own component

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,4 +1,5 @@
 import Sketch from "react-p5";
+import Info from "./Info";
 
 const Canvas = () => {
   const setup = (p5, canvasParentRef) => {
@@ -9,25 +10,10 @@ const Canvas = () => {
     p5.background(0, 0, 0);
   };
 
-  const umbrellaTag = (
-    <span className="uppercase font-varela text-yellow-300">umbrella</span>
-  );
-
   return (
     <div className="mx-auto max-w-7xl flex justify-center mt-20">
       <Sketch className="" setup={setup} draw={draw} />
-      <div className="ml-20 text-white font-noto">
-        <h1>Welcome to {umbrellaTag}.</h1>
-        <p className="mt-8">An experiment in generative art.</p>
-        <p className="mt-4">
-          Press the Generate button to create an art piece through the{" "}
-          {umbrellaTag} algorithm.
-        </p>
-        <p className="mt-4">No two generations are the same. Enjoy.</p>
-        <button className="w-full mt-8 border-white border-2 py-2 rounded hover:border-yellow-300 hover:text-yellow-300 transition">
-          <p className="text-base">Generate</p>
-        </button>
-      </div>
+      <Info />
     </div>
   );
 };
---
+++ src/components/Info.js
@@ -0,0 +1,22 @@
+const Info = () => {
+  const umbrellaTag = (
+    <span className="uppercase font-varela text-yellow-300">umbrella</span>
+  );
+
+  return (
+    <div className="ml-20 text-white font-noto">
+      <h1>Welcome to {umbrellaTag}.</h1>
+      <p className="mt-8">An experiment in generative art.</p>
+      <p className="mt-4">
+        Press the Generate button to create an art piece through the{" "}
+        {umbrellaTag} algorithm.
+      </p>
+      <p className="mt-4">No two generations are the same. Enjoy.</p>
+      <button className="w-full mt-8 border-white border-2 py-2 rounded hover:border-yellow-300 hover:text-yellow-300 transition">
+        <p className="text-base">Generate</p>
+      </button>
+    </div>
+  );
+};
+
+export default Info;
